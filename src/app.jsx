7bf22e158eab26c3b5d671c922b09ad61579171b
Steven Brown <Stevenjbrown95@gmail.com>
2025-02-13 18:15:24 -0700

    header and footer on app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">
+    <header>
+      <h1>Welcome to Card Cash</h1>
+      <hr />
+    </header>
+
+    <main>App componets go here</main>
+
+    <footer>
+      <div id="footer-info">
+        <a href="https://github.com/stevenjb5M/startup.git">Steven Brown's GitHub</a>
+      </div>
+      
+    </footer>
+
+
+
+  </div>;
+}
