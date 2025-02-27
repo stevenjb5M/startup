eaca00df8eabb6216d37ea504050f8f793697c03
Steven Brown <Stevenjbrown95@gmail.com>
2025-02-27 16:52:42 -0700

    locations page

--- src/locations/locations.jsx
+++ src/locations/locations.jsx
@@ -1,16 +1,23 @@
 import React, { useContext, useEffect, useState } from 'react';
 import "./locations.css";
 import "../main.css";
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 
 export function Locations() {
   const [locations, setLocations] = useState([]);
   const { currentUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
 
   useEffect(() => {
-    const storedLocations = JSON.parse(localStorage.getItem(currentUser + '/locations')) || [];
+    if (!currentUser.email) {
+      navigate('/');
+    }
+
+    const storedLocations = JSON.parse(localStorage.getItem(currentUser.email + '/locations')) || [];
     setLocations(storedLocations);
-  }, []);
+  }, [currentUser, navigate]);
 
   return (
     <main>
@@ -18,16 +25,17 @@ export function Locations() {
         <h2 id="locations-title">Your Locations</h2>
         <ul id="locations-list">
           {locations.map((location, index) => (
-            <li key={index} className="location">{location}</li>
+            <div className="specific-location-div">
+              <li key={index} className="location">{location}</li>
+              <button onClick={() => deleteLocation(index)}>Delete</button>
+            </div>
+            
           ))}
         </ul>
 
         <div id="add-remove-buttons">
           <a className="icon-link" href="#" onClick={openPopup}>
             <img id="plus-button" className="icon-button" src="plus-solid.svg" alt="Description of image" />
-          </a>
-          <a className="icon-link" href="">
-            <img className="icon-button" src="minus-solid.svg" alt="Description of image" />
           </a>
         </div>
       </div>
@@ -36,7 +44,7 @@ export function Locations() {
 
       <div id="popup" style={{ display: 'none' }}>
         <h3>Enter Location Name</h3>
-        <input type="text" id="locationName" placeholder="Location name" />
+        <input type="text" id="location-name" placeholder="Location name" />
         <br />
         <button type="button" onClick={addLocation}>Add Location</button>
         <button type="button" onClick={closePopup}>Close</button>
@@ -45,14 +53,20 @@ export function Locations() {
   );
 
   function addLocation() {
-    const locationName = document.getElementById('locationName').value;
+    const locationName = document.getElementById('location-name').value;
     if (locationName) {
       const updatedLocations = [...locations, locationName];
       setLocations(updatedLocations);
-      localStorage.setItem(currentUser + '/locations', JSON.stringify(updatedLocations));
-      document.getElementById('locationName').value = '';
+      localStorage.setItem(currentUser.email + '/locations', JSON.stringify(updatedLocations));
+      document.getElementById('location-name').value = '';
       closePopup();
     }
+  }
+
+  function deleteLocation(locationIndex) {
+    const updatedLocations = locations.filter((_, index) => index !== locationIndex);
+    setLocations(updatedLocations);
+    localStorage.setItem(currentUser.email + '/locations', JSON.stringify(updatedLocations));
   }
 
   function openPopup() {
